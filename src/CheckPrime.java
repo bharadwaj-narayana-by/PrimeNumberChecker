7dc5856a3a371913c39606eb2333fc188e3eb144
Bharadwaj <Bharadwaj.Patnadkanarayana@jda.com>
2022-01-17 07:56:50 +0530

    PrimeNumberChecker updated format

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -5,14 +5,15 @@ import java.util.stream.Collectors;
 public class CheckPrime {
 
     public static void main(String[] args) {
-        List<Integer> numToCheck = Arrays.asList(1,2,3,5,6,12,13);
+        List<Integer> numToCheck = Arrays.asList(1, 2, 3, 5, 6, 12, 13);
         List<Integer> primeNo = numToCheck.stream().filter(CheckPrime::isPrime).collect(Collectors.toList());
         System.out.println(primeNo);
     }
+
     private static Boolean isPrime(Integer x) {
         if (x <= 1) {
             return false;
-        }else {
+        } else {
             for (int i = 2; i < Math.sqrt(x); i++) {
                 if (x % i == 0) {
                     return false;
