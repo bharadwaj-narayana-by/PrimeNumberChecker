0369dff1607b096cac6231161a1ac96c2febcf8a
Bharadwaj <Bharadwaj.Patnadkanarayana@jda.com>
2022-01-17 07:51:07 +0530

    PrimeNumberChecker updated

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -6,20 +6,19 @@ public class CheckPrime {
 
     public static void main(String[] args) {
         List<Integer> numToCheck = Arrays.asList(1,2,3,5,6,12,13);
-        List<Integer> primeNo = numToCheck.stream().filter(x -> {
-            if (x <= 1) {
-                return false;
-            }else {
-                for (int i = 2; i < Math.sqrt(x); i++) {
-                    if (x % i == 0) {
-                        return false;
-                    }
-                }
-
-            }
-            return true;
-
-        }).collect(Collectors.toList());
+        List<Integer> primeNo = numToCheck.stream().filter(CheckPrime::isPrime).collect(Collectors.toList());
         System.out.println(primeNo);
+    }
+    private static Boolean isPrime(Integer x) {
+        if (x <= 1) {
+            return false;
+        }else {
+            for (int i = 2; i < Math.sqrt(x); i++) {
+                if (x % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
